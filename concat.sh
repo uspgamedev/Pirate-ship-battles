4585a891453e8a5a2c5d5897ba0089d310a81bc4
Zeke <vitinho_cga@hotmail.com>
2018-11-26 16:32:30 -0200

    Changes serve behavior and adds a new/better script to minifiy.

---
+++ concat.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+files="client/util.js client/players.js client/hud.js client/item.js client/login.js client/main.js client/game.js"
+target=${@: -1}
+
+if [ $# -lt 2 ]
+then
+    cat $files > $target
+else
+    files=${@:1:$(($#-1))}
+    cat $files > $target
+fi
+
+files=$(sed 's/\//\\\//g' <<< $files)
+
+files=($files)
+
+sed -i 's/'$files'/'$target'/' index.html 
+
+i=$(( 1 ))
+while [ $i -lt ${#files[@]} ]
+do
+    a=${files[$i]}
+    sed -i '/'$a'/d' index.html
+    i=$((i+1)) 
+done
